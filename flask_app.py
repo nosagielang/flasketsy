0776797cc2ea042e2c83eb4e82f3980693b01ded
Nosagie Lang <nosagie.lang@gmail.com>
2017-03-10 08:30:33 +0000

    Add Style Page

--- flask_app.py
+++ flask_app.py
@@ -74,6 +74,12 @@ def listing_edit(listing_id):
             abort(404)
         return render_template('newlisting.html', form=lstForm)
 
+@app.route('/listing/delete/<listing_id>', methods = ['GET','POST'])
+def listing_delete(listing_id):
+    Listing.query.filter_by(id=listing_id).delete()
+    db.session.commit()
+    return render_template('listing_delete_confirm.html', listingid=listing_id)
+
 @app.route('/about')
 def about():
     return render_template('about.html')
@@ -81,4 +87,8 @@ def about():
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
+
+@app.route('/TermsConditions')
+def TermsConditions():
+    return render_template('TermsConditions.html')
 
